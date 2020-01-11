3cb109d95af04d1ec273fac9603de87d44596c68
Leonardo Nguyen <leonguyen8@yahoo.com>
2020-01-10 17:13:22 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+    }
+ 
+    @Override
+    public void robotPeriodic() {
+    }
+
+    @Override
+    public void autonomousInit() {
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+  
+    @Override
+    public void testPeriodic() {
+    }
+}
