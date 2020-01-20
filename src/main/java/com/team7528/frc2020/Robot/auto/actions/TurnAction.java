b3222c4ecd581c8a8be55523dd4b0f37ed65cc5c
Matthew Correia <100518@students.mhusd.org>
2020-01-20 13:55:56 -0800

    Forgot some comments

--- src/main/java/com/team7528/frc2020/Robot/auto/actions/TurnAction.java
+++ src/main/java/com/team7528/frc2020/Robot/auto/actions/TurnAction.java
@@ -8,7 +8,7 @@ import static com.team7528.frc2020.Robot.common.RobotMap.m_drive;
  */
 public class TurnAction implements Action {
 
-    private final double kP = 0.07;
+    private final double kP = 0.07; // P constant
     private double desiredAngle; // The angle we want
     private double turn_power; // How much power to turn with
     private double error;  // The difference between the desired angle and the difference between the current angle and the starting angle
