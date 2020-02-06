6417ab857afbf738dab7a648b6b8d4bd2e73eb48
Luis Carrillo <65725@students.mhusd.org>
2020-02-05 18:53:42 -0800

    Unfinished work on turret code

--- src/main/java/com/team7528/frc2020/Robot/components/Turret.java
+++ src/main/java/com/team7528/frc2020/Robot/components/Turret.java
@@ -1,30 +1,86 @@
 package com.team7528.frc2020.Robot.components;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
-import static com.team7528.frc2020.Robot.common.RobotMap.m_gamepad;
+import static com.team7528.frc2020.Robot.common.RobotMap.*;
+
+/**
+ * Code will operate by spinning either left or right until a target is detected
+ */
 
 public class Turret {
 
+    private static StringBuilder stats = new StringBuilder(); // StringBuilder for statistics
+    private static double currentRPM;
+    private static int loopCount;
+    private static double desiredRPM;
     private boolean seek_r;
     private boolean seek_l;
+    private boolean disengage;
+
+    public void init() {
+        loopCount = 0; // Resets the loopCount
+        desiredRPM = 1000; // Sets the desired RPM
+    }
+
+    /**
+     * This code will be used to set up how much power will be use to spin and what buttons will be used to operate turret
+     */
 
     public void periodic() {
 
         double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
         double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
 
-        if (tv == 1) {
+        // Setting up the rotation of turret
+
+        if (currentRPM == desiredRPM) {
+            if (tv == 1) {
+                drumSpinner.configFactoryDefault();
+            } else if (tv == 0) {
+                drumSpinner.getMotorOutputPercent();
+            }
+            loopCount++; // Increments loopCount
+            if (loopCount >= 10) {
+                reportStatistics(); // Reports statistics
+                loopCount = 0; // Reset the loopCount
+            }
+
+            // Setup for seeking right, left, and disengaging
+
+            if (m_gamepad.getBButtonPressed()) { // right
+                seek_r = true;
+            }
+
+            if (seek_r) {
+                drumSpinner.configFactoryDefault();
+            } else {
+                drumSpinner.setInverted(true);
+            }
+
+            if (m_gamepad.getXButtonPressed()) { // left
+                seek_l = true;
+            }
+
+            if (seek_l) {
+                drumSpinner.configFactoryDefault();
+            } else {
+                drumSpinner.setInverted(true);
+            }
+
+            if (m_gamepad.getAButtonPressed()) { // disengage
+                disengage = true;
+            }
+
         }
 
-        if (m_gamepad.getBButtonPressed()) {
-            seek_r = true;
-        } else
-        if (m_gamepad.getXButtonPressed()) {
-            seek_l = true;
-        }
-        if (m_gamepad.getAButtonPressed()) {
-        }
+    }
 
+    // printing out statistics
+
+    public static void reportStatistics() {
+        stats.append("Current turret RPM: ").append(currentRPM);
+        System.out.println();
+        stats.setLength(0);
     }
 
 }
