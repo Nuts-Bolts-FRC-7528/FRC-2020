3c7eaf32fc00fb6268405e10cfa82c34ea871fa8
Matthew Correia <100518@students.mhusd.org>
2020-02-14 20:44:14 -0800

    Modified control panel code

--- src/main/java/com/team7528/frc2020/Robot/components/ControlPanel.java
+++ src/main/java/com/team7528/frc2020/Robot/components/ControlPanel.java
@@ -8,40 +8,38 @@ import static com.team7528.frc2020.Robot.common.RobotMap.*;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.util.Color;
 
-public class ControlPanel {/*
+public class ControlPanel {
     public static void init() {
 
     }
     public static void periodic() {
 
         if(m_gamepad.getPOV() ==180) { // D-Pad down
-            controlPanelWheel.set(ControlMode.PercentOutput,.5); // Spins Control Panel Wheels at 50% power
-// If gameData = 0, then still in spin mode; otherwise it tells what color we've been assigned. Write code to determine color and stop
             String gameData;
             gameData = DriverStation.getInstance().getGameSpecificMessage(); //Add method to determine color
             if(gameData.length() > 0) {
                 switch (gameData.charAt(0)) {
                     case 'B' :
                         //Blue case code
-                        if (colors.getBlue() <200) {
+                        if (colors.getColor() != Color.kBlue) {
                             controlPanelWheel.set(ControlMode.PercentOutput,1);
                         }
                         break;
                     case 'G' :
                         //Green case code
-                        if (colors.getGreen() <200) {
+                        if (colors.getColor() != Color.kGreen) {
                             controlPanelWheel.set(ControlMode.PercentOutput,1);
                         }
                         break;
                     case 'R' :
                         //Red case code
-                        if (colors.getRed() <200) {
+                        if (colors.getColor() != Color.kRed) {
                             controlPanelWheel.set(ControlMode.PercentOutput,1);
                         }
                         break;
                     case 'Y' :
                         //Yellow case code
-                        if (colors.getColor() == Color.kYellow) {
+                        if (colors.getColor() != Color.kYellow) {
                             controlPanelWheel.set(ControlMode.PercentOutput,1);
                         }
                         break;
@@ -50,8 +48,10 @@ public class ControlPanel {/*
                         System.out.println("This is corrupt data");
                         break;
                 }
-            } else {
-                //Code for no data received yet
+            } else if (colors.getColor() == Color.kYellow);{
+                controlPanelWheel.set(ControlMode.PercentOutput,1);
+                
+
             }
         }
         if(m_gamepad.getPOV() == 0) { // D-Pad Up
@@ -66,5 +66,5 @@ public class ControlPanel {/*
     public static void reportSatistics() {
 
     }
-*/
+
 }
