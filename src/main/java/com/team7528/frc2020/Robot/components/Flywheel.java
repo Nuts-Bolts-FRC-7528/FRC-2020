f86837998873fb223cb5dec5f4cea6863e590d38
Ethan Hanlon <67823@students.mhusd.org>
2020-03-04 19:31:03 -0800

    Code cleanup

--- src/main/java/com/team7528/frc2020/Robot/components/Flywheel.java
+++ src/main/java/com/team7528/frc2020/Robot/components/Flywheel.java
@@ -3,152 +3,132 @@ package com.team7528.frc2020.Robot.components;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import static com.team7528.frc2020.Robot.common.RobotMap.*;
 
 /**
- * Class for the flywheel component
+ * Class for the flywheel component. Also handles calculating the distance to the target using a Limelight.
+ * Note that the flywheel encoder velocity has to be reversed each time we use it, as it is out of phase with
+ * the direction it is actually moving in. The setPhase() method appears to do nothing in this regard, so we
+ * simply need to invert it each time it is used.
  *
  * @author Matthew Correia
  */
 @SuppressWarnings({"FieldCanBeLocal"})
 public class Flywheel {
 
-    private static final double a1 = 37.5; // The angle the limelight is mounted at
-    private static final double h1 = 30 /* Non-block Height: 11.25 */ ; // The height the limelight is mounted at
-    private static final double h2 = 98.25; // The height of the target
-    private static final double k_distance = 20000/112.0; // Constant for the flywheel speed
-    private static final double k_flywheelTolerance = 100; // The flywheel tolerance
-    private static final double k_gearRatio = 1 / 4.0; // The gear ratio for the flywheel
-    private static final double kP = 0.0001; // P constant for PID loop
-    //Ku = 0.1, 0.06
-    //Tu = 0.3
-    private static final double kI = 0.007/*2*/; // I constant for PID loop
-    private static final double kD = 0.0/*0.0012659625*/; // D constant for PID loop
-    private static final double kF = 0.005;
-    private static double a2; // The angle from the limelight
-    private static double currentRPM; // The flywheel's current RPM
-    public static double d; // The distance to the target
-    private static double desiredRPM; // As it says, the desired RPM
-    private static double error; // The difference between desiredRPM & currentRPM
-    private static double errorRate; // The rate of change for the error
-    private static double errorSum; // The sum of the errors
-    private static double previousError; // The previous iteration's error
-    private static double speed; // The speed to set for the flywheel motor
-    private static int loopCount; // Helps us print statistics 5 times per second
-    private static int shootingLoop, shootingLoopTwo;
-    private static StringBuilder stats = new StringBuilder(); // StringBuilder for statistics
-    private static double encoderVelocity; // the velocity of the flywheel
-    private static double dips; // the amount the rpm dips
-    public static int ballAmount; // The amount of balls in the flywheel
+    /*   [DISTANCE CONSTANTS]   */
+    private static final double a1 = 37.5; //The angle the limelight is mounted at
+    private static final double h1 = 30; //The height the limelight is mounted at
+    private static final double h2 = 98.25; //The height of the target
+
+    /*   [FLYWHEEL PIDF CONSTANTS]   */
+    private static final double kP = 0.0001; //P constant for PID loop
+    private static final double kI = 0.007; //I constant for PID loop
+    private static final double kD = 0.0; //D constant for PID loop
+    private static final double kF = 0.005; //Feedforward Setpoint for PID loop
+    private static final double kIntegratorZone = 1000; //Within this many ticks of the setpoint, errorSum will increase
+    private static final double k_flywheelTolerance = 100; //The flywheel tolerance
+    private static final double velocitySetpoint = 20000; //Desired encoder velocity
+
+    /*   [VARIABLES]   */
+    public static double d; //The distance to the target
+    private static double a2; //The angle from the limelight
+    private static double error; //The difference between desiredRPM & currentRPM
+    private static double errorRate; //The rate of change for the error
+    private static double errorSum; //The sum of the errors
+    private static double previousError; //The previous iteration's error
+    private static double speed; //The speed to set for the flywheel motor
 
     /**
-     * Resets the iteration counter (loopCount)
+     * Sets Phoenix motor settings
      */
     public static void init() {
-        loopCount = 0; // Resets the iteration counter (loopCount)
+        //Reset Flywheel Falcons to factory default
         flywheelMaster.configFactoryDefault();
         flywheelSlave.configFactoryDefault();
-        flywheelSlave.follow(flywheelMaster);
-        flywheelSlave.setInverted(true);
-        flywheelMaster.setNeutralMode(NeutralMode.Brake);
-        flywheelSlave.setNeutralMode(NeutralMode.Brake);
-//        flywheelMaster.setSensorPhase(true);
-        flywheelMaster.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
-        flywheelMaster.setSelectedSensorPosition(0,0,10);
-//        flywheelMaster.configOpenloopRamp(0.5,10);
+
+        //Master settings
+        flywheelMaster.setNeutralMode(NeutralMode.Brake); //Set neutral mode to break
+        flywheelMaster.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor); //Use Falcon 500 Integrated Encoder
+        flywheelMaster.setSelectedSensorPosition(0,0,10); //Reset encoder position
+
+        //Slave settings
+        flywheelSlave.follow(flywheelMaster); //Follow master mode
+        flywheelSlave.setInverted(true); //Invert motor to go along with master controller
+        flywheelSlave.setNeutralMode(NeutralMode.Brake); //Set neutral mode to break
     }
 
     /**
      * Shoots the ball when start is pressed and prints statistics
      */
     public static void periodic() {
-        a2 = limelightTable.getEntry("ty").getDouble(0); // Sets a2, the y position of the target
-        currentRPM = flywheelMaster.getSelectedSensorVelocity() / k_gearRatio; // Gets the flywheel's current RPM
-        d = Math.round((h2-h1) /* 2.56*/ / Math.tan(Math.toRadians(a1+a2))); // Finds the distance
+        a2 = limelightTable.getEntry("ty").getDouble(0); //Sets a2, the y position of the target
+        d = Math.round((h2-h1) / Math.tan(Math.toRadians(a1+a2))); //Calculates distance using a tangent
 
-        if(m_gamepad.getStartButtonReleased()) {
+        if(m_gamepad.getStartButtonReleased()) { //Reset errorSum to 0 when start is released
             errorSum = 0;
         }
 
-        shooting();
-        reportStatistics();
-
-//        flywheelMaster.set(ControlMode.PercentOutput, m_gamepad.getY(GenericHID.Hand.kRight));
+        shooting(); //Determine when we should be shooting
+        reportStatistics(); //Report telemetry to the Driver Station
     }
 
     /**
      * Sets up shooting when we are holding start
      */
     private static void shooting() {
-        if (m_gamepad.getStartButton()) { // If the start button is pressed ...
-            if (Math.abs(desiredRPM - -flywheelMaster.getSelectedSensorVelocity()) <= k_flywheelTolerance) { // ... and we're close enough to the desired RPM ...
-                SmartDashboard.putBoolean("AUTO SHOOT READY", true);
-                shoot(); // ... then shoot
-            } else {
-                SmartDashboard.putBoolean("AUTO SHOOT READY", false);
+        if (m_gamepad.getStartButton()) { //If the start button is pressed
+            if (Math.abs(velocitySetpoint - -flywheelMaster.getSelectedSensorVelocity()) <= k_flywheelTolerance) { // ... and we're close enough to the desired RPM ...
+                SmartDashboard.putBoolean("AUTO SHOOT READY", true); //Alert driver that the shooting is ready
+                runConveyor(); //Convey balls into the flywheel
+            } else { //If we are NOT within our tolerance
+                SmartDashboard.putBoolean("AUTO SHOOT READY", false); //Alert the operator
             }
-            PID();
-            flywheelMaster.set(ControlMode.PercentOutput, -speed); // ... set the motor speed to the desired RPM
-            shootingLoop++;
-        } else {
-            flywheelMaster.set(ControlMode.PercentOutput, m_gamepad.getY(GenericHID.Hand.kLeft));
-            SmartDashboard.putBoolean("AUTO SHOOT READY", false);
+            PID(); //Calculate PIDF loop
+            flywheelMaster.set(ControlMode.PercentOutput, -speed); //Run the motor at the calculated level
+        } else { //If start button is NOT pressed
+            flywheelMaster.set(ControlMode.PercentOutput, m_gamepad.getY(GenericHID.Hand.kLeft)); //Set flywheel manually with joystick
+            SmartDashboard.putBoolean("AUTO SHOOT READY", false); //Alert operator
         }
-        shootingLoopTwo++;
     }
 
     /**
-     * PID loop method
+     * Calculate PID outputs
      */
     private static void PID() {
-        error = (desiredRPM - -flywheelMaster.getSelectedSensorVelocity());
-        if(Math.abs(error) <= 1000) {
-//        if (errorSum >= 100) { // If the error sum gets too high ...
-//            errorSum = 100; // ... set it to the maximum
-//        } else if (errorSum <= -100) {
-//            errorSum = -100;
-//        }
-            errorSum += error * 0.02; // Add the error to the error sum
+        error = (velocitySetpoint - -flywheelMaster.getSelectedSensorVelocity()); //Calculate error
+        if(Math.abs(error) <= kIntegratorZone) { //If we are within our integrator zone
+            errorSum += error * 0.02; //Add the error to the error sum
         }
-        errorRate = (error - previousError) / 0.02;
-        speed = (kF * desiredRPM + kP * error + kI * errorSum + kD * errorRate) / 100.0;
-        if(speed < 0) {
-            speed = 0;
+        errorRate = (error - previousError) / 0.02; //Find the rate of change of the error
+        speed = (kF * velocitySetpoint + kP * error + kI * errorSum + kD * errorRate) / 100.0; //Calculate PIDF
+
+        if(speed < 0) { //If speed is negative
+            speed = 0; //Set speed to 0 (The flywheel should not run in reverse)
         }
-//        desiredRPM = k_distance*d; // Sets the desired RPM
-        desiredRPM = 20000; // Sets the desired RPM
-        previousError = error;
+
+        previousError = error; //Set previous error to the current error so we can use it in the next iteration
     }
 
     /**
-     * Reports the current flywheel RPM, then resets the String Builder (stats)
+     * Reports Telemetry to the Driver Station including the PIDF speed, setpoint, error, errorSum,
+     * and flywheel master motor encoder velocity
      */
     private static void reportStatistics() {
-        loopCount++;
-        if (loopCount >= 10) {
-            stats.append("Current flywheel RPM: ").append(currentRPM); // The current flywheel RPM
-//            System.out.println(stats); // Print the stats variable (current flywheel RPM)
-            stats.setLength(0); // And clear the stats variable to get it ready for the next print
-            loopCount = 0;
-        }
-        SmartDashboard.putBoolean("Gamepad Start button pressed", m_gamepad.getStartButton());
-        SmartDashboard.putNumber("Iterations with shooting() called", shootingLoopTwo);
-        SmartDashboard.putNumber("Iterations with shooting() used", shootingLoop);
         SmartDashboard.putNumber("Speed value", speed);
-        SmartDashboard.putNumber("Desired RPM", desiredRPM);
+        SmartDashboard.putNumber("Desired RPM", velocitySetpoint);
         SmartDashboard.putNumber("Error", error);
         SmartDashboard.putNumber("ErrorSum", errorSum);
+        SmartDashboard.putNumber("Flywheel Velocity (Master)", -flywheelMaster.getSelectedSensorVelocity());
     }
 
     /**
      * Shoots the ball
      */
-    public static void shoot() {
+    public static void runConveyor() {
         ballConveyor.set(0.2);
     }
 }
